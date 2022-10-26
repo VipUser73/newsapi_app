822c6e43abc40ccbd5abc1b75ac5900ff88a3c99
Evgeny Kolesnikov <vipuser73@gmail.com>
2022-10-26 18:06:25 +0400

    Update news KEY.

--- lib/services/news_api.dart
+++ lib/services/news_api.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as https;
 class NewsApi {
   Future<NewsModel> getNews(int page, String endpoint) async {
     final url = Uri.parse(
-        "https://newsapi.org/v2/$endpoint?q=Apple&pageSize=15&page=$page&apiKey=88967470461e47c2a24e5d6bc939c3f4");
+        "https://newsapi.org/v2/$endpoint?q=Apple&pageSize=15&page=$page&apiKey=43a3c1dad96e43238ed194a673a36511");
     final response = await https.get(url);
     var body = jsonDecode(response.body);
     return NewsModel.fromJson(body);
